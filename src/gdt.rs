43b3f9305d0ccfca8802330cbb350fc69427b3a1
WangWindow <1598593280@qq.com>
2024-12-24 21:26:48 +0800

    Fix some warnings.

--- src/gdt.rs
+++ src/gdt.rs
@@ -10,9 +10,10 @@ lazy_static! {
         let mut tss = TaskStateSegment::new();
         tss.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX as usize] = {
             const STACK_SIZE: usize = 4096 * 5;
-            static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
+            use spin::Mutex;
+            static STACK: Mutex<[u8; STACK_SIZE]> = Mutex::new([0; STACK_SIZE]);
 
-            let stack_start = VirtAddr::from_ptr(unsafe { &STACK });
+            let stack_start = VirtAddr::from_ptr(&STACK.lock() as *const _ as *const u8);
             let stack_end = stack_start + STACK_SIZE;
             stack_end
         };
